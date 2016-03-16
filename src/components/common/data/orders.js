7d53e474c0b7abdabf15fc11f37a5d85d2a69096
zhny <461787231@qq.com>
2016-03-16 18:05:17 +0800

    订单列表

---
+++ src/components/common/data/orders.js
@@ -0,0 +1,34 @@
+module.exports={
+	total:1,
+	counts:1,
+  	rows:[                                  
+	  	{
+	  		"orderId":"1",
+	  		"roomId":"1",
+	  		"guestName":"张三",
+	  		"phone":"136xxxxxx",
+	  		"channelName":"熟客及推介",
+	  		orderItems:[
+	  			{
+	  				"created":"2016-03-17",
+					"createDate":"2016-03-16",
+		  			"checkInDate":"2016-03-16",
+		  			"roomName":"呵呵房",
+		  			"price":"800",
+		  			createBy:"操作员",
+		  			orderItemStatus:2
+	  			},
+	  			{
+	  				"created":"2016-03-17",
+					"createDate":"2016-03-16",
+		  			"checkInDate":"2016-03-16",
+		  			"roomName":"哈哈房",
+		  			"price":"900",
+		  			createBy:"操作员",
+		  			orderItemStatus:2
+	  			},
+	  		]
+	  		
+	  	}
+  	]
+ }
