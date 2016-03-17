e8651421150e35556b6002a157a4ec216daf5414
fontland <fontlandbk@gmail.com>
2016-03-17 15:23:08 +0800

    dgeryttytrytr

---
+++ src/components/common/data/distorders.js
@@ -0,0 +1,389 @@
+module.exports={
+  	rows:[                                  
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  		
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  	
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  		
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  	
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  		
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  	
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  		
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  	
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  		
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  	
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  		
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  	
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  		
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  	
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  		
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  	
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  		
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  	
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  		
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  	
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  		
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  	
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  		
+	  	},
+	  	{
+	  		"orderId":2,						//订单ID
+			"orderCreateDate":"2016-02-20",		//订单创建时间
+			"orderAmount":5000,					//订单金额	
+			"hotelId":3,						//客栈ID
+			"hotelName":"翰林一号院",
+			"orderDesc":"翰院房(秋波媚) 张三,13547695555,2016-02-20入住，住2晚，订单金额200;翰院房(迷神引) 李四,13547695555,2016-02-20入住，住2晚，订单金额300",
+			"channelCode":"ddd",				//分销渠道ID
+			"channelName":"张三",				//分销渠道名称
+			"rebate":0.05,						//返点比例5%
+			"rebateAmount":223,					//返点金额
+			"orderStatus": "1",                  //0 未完成 1已完成
+			"rebateStatus":"0",					//返点状态:   0 未返点，1 已返点
+			"rebateDate":"2016-02-20",			//饭店时间
+			"userName":"李东汉"					//操作人名称	  	
+	  	},
+  	]
+ }
+
